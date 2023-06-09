d3657727c296aa58cc886c6ccb6407b8109ff58b
jd0097 <dh.react123@gmail.com>
2023-06-09 15:59:55 +0900

    feat: 기본 페이지 구성 및 Router, Bootstrap 적용

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = ({title}) => {
+  
+  return (
+    <div className='card card-body'> 
+    <h2>About {title} </h2>
+    </div>
+  )
+}
+
+export default About
